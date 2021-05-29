1825ee6b432975435595301788d74b545a3b339b
babs12316 <44439609+babs12316@users.noreply.github.com>
2021-05-30 00:22:45 +0200

    Folder restructring for better organization of files

---
+++ src/views/buyFlow/Buyflow.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import AgeStep from '../../components/ageStep/AgeStep'
+import EmailStep from '../../components/emailStep/EmailStep'
+import SummaryStep from '../../components/summaryStep/SummaryStep'
+
+interface BuyflowProps {
+  productId: ProductIds
+}
+
+export enum ProductIds {
+  // eslint-disable-next-line no-unused-vars
+  devIns = 'dev_ins',
+}
+
+const PRODUCT_IDS_TO_NAMES = {
+  [ProductIds.devIns]: 'Developer Insurance',
+}
+
+const Buyflow: React.FC<BuyflowProps> = (props) => {
+  const [currentStep, setStep] = useState('email')
+  const [collectedData, updateData] = useState({
+    email: '',
+    age: 0,
+  })
+  const getStepCallback = (nextStep: string) => (field: string, value: any) => {
+    updateData({ ...collectedData, [field]: value })
+    setStep(nextStep)
+  }
+  return (
+    <>
+      <h4>Buying {PRODUCT_IDS_TO_NAMES[props.productId]}</h4>
+      {(currentStep === 'email' && <EmailStep cb={getStepCallback('age')} />) ||
+        (currentStep === 'age' && (
+          <AgeStep cb={getStepCallback('summary')} />
+        )) ||
+        (currentStep === 'summary' && (
+          <SummaryStep collectedData={collectedData} />
+        ))}
+    </>
+  )
+}
+
+export default Buyflow
